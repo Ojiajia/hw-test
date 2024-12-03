26cb7bb4a253a5eb4ad5522102bdbbf12f92bfd4
Коростелина Ольга Юрьевна <okorostelina@elvees.com>
2024-12-03 14:06:34 +0300

    HW2 is completed

--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -1,30 +1,41 @@
 package reader
 
-import	"encoding/json"
-import	"fmt"
-import	"io"
-import	"os"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
 
-import	"github.com/fixme_my_friend/hw02_fix_app/types"
-
+	"github.com/fixme_my_friend/hw02_fix_app/types"
+)
 
 func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
+		return nil, err
 	}
 
-	byte, err := io.ReadAll(f)
+	vsbyten, err := io.ReadAll(f)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
-		return nil, nil
+		// return nil, nil
+		return nil, err
 	}
 
 	var data []types.Employee
 
-	err = json.Unmarshal(bytes, &data)
+	err = json.Unmarshal( // from JSON 2 &data
+		vsbyten,
+		&data,
+	)
 
-	res := data
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return nil, err
+	}
 
-	return res, nil
+	//res := data
+
+	return data, err
 }
