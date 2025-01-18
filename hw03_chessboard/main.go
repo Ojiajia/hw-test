df25b71399668b490908e38a0914ca9df6f9b298
okorostelina <okorostelina@elvees.com>
2025-01-18 16:56:09 +0300

    HW3 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+
+var m,n int
+
+fmt.Print("Type two numbers:")
+
+fmt.Scan(&m, &n)
+
+for i := 0; i < m; i++ {
+
+	for j := 0; j < n; j++ {
+
+	if ((j % 2) == 0) && ((i % 2) == 0) {
+
+		fmt.Printf("\x23")
+
+		} else if ((j % 2) != 0) && ((i % 2) != 0) {
+
+			fmt.Printf("\x23")
+
+		} else {
+
+				fmt.Printf(" ")
+
+		}
+
+	}
+
+fmt.Printf("\n")
+
+}
+
 }
