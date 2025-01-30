ac6de2d207ef77121f32fa140d6c64b37ac8cc63
Ojiajia <bluegraycup@gmail.com>
2025-01-30 20:39:12 +0300

    HW4 is completed(added new features)

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -23,15 +23,66 @@ func (booBook *Book) Set(bId int, bTitle, bAuth string, bYear int, bSize int, bR
 	return booBook
 }
 
-func (booBook *Book) Get(Book) {
+func (booBook Book) Get(Book) {
 
-	print("Id: ", booBook.ID, " Title: ", booBook.Title, " Author: ", booBook.Author, " Year: ", booBook.Year, " Size: ", booBook.Size, " Rate: ", booBook.Rate)
+	print("Id: ", booBook.ID, " Title: ", booBook.Title, " Author: ", booBook.Author, " Year: ", booBook.Year, " Size: ", booBook.Size, " Rate: ", booBook.Rate, "\n")
+
+}
+
+type Mode int
+
+const (
+	YearMode Mode = iota + 1
+	SizeMode
+	RateMode
+)
+
+type ModeVariety struct {
+	Choice Mode
+}
+
+func (m ModeVariety) ToCompare(first Book, second Book) bool {
+
+	switch m.Choice {
+	case 1:
+		print("you choose Year\n")
+		if first.Year > second.Year {
+			print("true\n")
+			return true
+		} else {
+			print("false\n")
+			return false
+		}
+	case 2:
+		print("you choose Size\n")
+		if first.Size > second.Size {
+			print("true\n")
+			return true
+		} else {
+			print("false\n")
+			return false
+		}
+	case 3:
+		print("you choose Rate\n")
+		if first.Rate > second.Rate {
+			print("true\n")
+			return true
+		} else {
+			print("false\n")
+			return false
+		}
+	default:
+		print("something's wrong\n")
+
+	}
+
+	return true // ?
 
 }
 
 func main() {
 
-	print("Input your Book data: ID, Title, Author, Year, Size, Rate\n")
+	print("Input your first Book data: ID, Title, Author, Year, Size, Rate\n")
 
 	var A, D int
 	var B, C string
@@ -40,9 +91,29 @@ func main() {
 
 	fmt.Scan(&A, &B, &C, &D, &E, &F)
 
-	var BiBook Book
+	var BookOne Book
 
-	BiBook.Set(A, B, C, D, E, F)
+	BookOne.Set(A, B, C, D, E, F)
 
-	BiBook.Get(BiBook)
+	A = 0
+	B = ""
+	C = ""
+	D = 0
+	E = 0
+	F = 0.
+
+	print("Input your second Book data: ID, Title, Author, Year, Size, Rate\n")
+
+	fmt.Scan(&A, &B, &C, &D, &E, &F)
+
+	var BookTwo Book
+
+	BookTwo.Set(A, B, C, D, E, F)
+
+	var CompData ModeVariety
+	print("Input mode for comparison (1 = Year, 2 = Size, 3 = Rate)\n")
+	fmt.Scan(&CompData.Choice)
+
+	CompData.ToCompare(BookOne, BookTwo)
+
 }
