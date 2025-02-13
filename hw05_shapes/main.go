2dda0ea360d0a5e8133ddc8bacc228f5146d6750
Ojiajia <bluegraycup@gmail.com>
2025-02-13 17:14:03 +0300

    HW5 is completed

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -14,74 +14,49 @@ type Rectangle struct { // прямоугольник
 	A, B float32 // A - длина, B - ширина
 }
 
-//**********
-
-type VarShape struct { // для выбора структуры
-	Circle
-	Triangle
-	Rectangle
-}
-
-type Mode int // выбор варианта фигуры для подсчета площади
-
-const (
-	CircleMode    Mode = iota + 1 // считаем площадь круга
-	TriangleMode                  // считаем площадь треугольника
-	RectangleMode                 // считаем площадь прямоугольника
-)
-
-type ModeVariety struct {
-	Choice Mode
-}
 type Area interface {
-	CalcArea(VarShape) float32
+	CalcArea() float32
 }
 
 //**********
 
-func (m ModeVariety) CalcArea(someShape VarShape) float32 { // - ?
+func (c Circle) CalcArea() float32 {
+	print("Circle Area =\n")
+	return (math.Pi) * (c.R) * (c.R)
+}
 
-	switch m.Choice { // выберем фигуру для посдчета площади
-	case 1:
-		print("you choose Circle\n")
-		return math.Pi * (someShape.Circle.R) * (someShape.Circle.R)
-	case 2:
-		print("you choose Triangle\n")
-		return someShape.Triangle.H * someShape.Triangle.L / 2
-	case 3:
-		print("you choose Rectangle\n")
-		return someShape.Rectangle.A * someShape.Rectangle.B
-	default:
-		print("you were wrong")
-		return 0
-	}
+func (t Triangle) CalcArea() float32 {
+	print("Triangle Area =\n")
+	return t.H * t.L / 2
+}
+
+func (r Rectangle) CalcArea() float32 {
+	print("Rectangle Area =\n")
+	return r.A * r.B
 }
 
 //**********
+
+func CallInterface(a Area) {
+
+	print(a.CalcArea())
+
+}
 
 func main() {
 
-	var SomeVar VarShape
-	var ChoosedVar ModeVariety
-	var a Area
+	var c Circle
+	c.R = 3.
+	CallInterface(c)
 
-	SomeVar.Circle = Circle{3.}
-	ChoosedVar.Choice = 1 // площадь круга
+	var t Triangle
+	t.H = 2.
+	t.L = 3.
+	CallInterface(t)
 
-	ChoosedVar.CalcArea(SomeVar)
-
-	a.CalcArea(SomeVar)
-
-	print("Circle with R = 3 area = ", a.CalcArea(SomeVar))
-	// print("Circle with R = 3 area = ", a)
-
-	SomeVar.Triangle = Triangle{5., 4.}
-	ChoosedVar.Choice = 2 // площадь треугольника
-	a.CalcArea(SomeVar)
-	print("Triangle with H = 5, L = 4, area = ", a.CalcArea(SomeVar))
-
-	SomeVar.Rectangle = Rectangle{2, 3}
-	ChoosedVar.Choice = 3 // площадь прямоугольника
-	print("Rectangle with A = 2, B = 3, area = ", a.CalcArea(SomeVar))
+	var r Rectangle
+	r.A = 5.
+	r.B = 4.
+	CallInterface(r)
 
 }
