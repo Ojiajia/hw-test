729c25d04e568513379bdb232a7e19705bc10d6c
Ojiajia <bluegraycup@gmail.com>
2025-02-14 19:21:18 +0300

    HW5 is completed again

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -18,45 +18,52 @@ type Area interface {
 	CalcArea() float32
 }
 
-//**********
-
 func (c Circle) CalcArea() float32 {
-	print("Circle Area =\n")
 	return (math.Pi) * (c.R) * (c.R)
 }
 
 func (t Triangle) CalcArea() float32 {
-	print("Triangle Area =\n")
 	return t.H * t.L / 2
 }
 
 func (r Rectangle) CalcArea() float32 {
-	print("Rectangle Area =\n")
 	return r.A * r.B
 }
 
-//**********
-
 func CallInterface(a Area) {
-
 	print(a.CalcArea())
-
 }
 
 func main() {
 
 	var c Circle
 	c.R = 3.
+	print("Circle Area = ")
 	CallInterface(c)
 
 	var t Triangle
 	t.H = 2.
 	t.L = 3.
+	print("\nTriangle Area = ")
 	CallInterface(t)
 
 	var r Rectangle
 	r.A = 5.
 	r.B = 4.
+	print("\nRectangle Area = ")
 	CallInterface(r)
 
+	shapes := []Area{c, t, r}
+	for _, shape := range shapes {
+		switch v := shape.(type) {
+		case Circle:
+			print("\nit's a circle, area = ", v.CalcArea())
+		case Triangle:
+			print("\nit's a triangle, area = ", v.CalcArea())
+		case Rectangle:
+			print("\nit's a rectangle, area = ", v.CalcArea(), "\n")
+		default:
+			print("\nuknown shape type\n")
+		}
+	}
 }
